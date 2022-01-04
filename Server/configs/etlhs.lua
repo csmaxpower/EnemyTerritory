cfb4ca211cb030a4c0825617921f256143be1cb9
Rand Harris <30740124+randharris@users.noreply.github.com>
2022-01-04 10:59:44 -0600

    updates for legacy

--- Server/configs/etlhs.lua
+++ Server/configs/etlhs.lua
@@ -4,7 +4,7 @@ local Description = "^7Hide^1&^7Seek for ETLegacy"
 local Homepage = "https://discord.moestavern.pub"
 
 
-et.NUM_OF_WEAPONS = 56
+et.MAX_WEAPONS = 56
 samplerate = 200
 
 
@@ -67,14 +67,166 @@ local weapons = {
 	false   --et.WP_AIRSTRIKE	55	Airstrike
 }
 
+maxclients = 0
+gamestate = 0
+gameActive = false
+freezeTime = 0
+startTime = 0
+antigibEnable = true
+clientSpawnOrigins = { }
+alliesReleasedMessage = false
+alliedMaxHp = 300
+axisMaxHp = 151
+
+for k=0,63 do clientSpawnOrigins[k] = 0 end
+
 function et_InitGame(levelTime,randomSeed,restart)
 	maxclients = tonumber( et.trap_Cvar_Get( "sv_maxClients" ) )   --gets the maxclients
-	et.G_Print("Hide & Seek Version:"..Version.." Loaded\n")
-   	et.RegisterModname("ETL Hide & Seek by Moe's")
-	et.trap_SendServerCommand(-1, "b 8 \""..Description.." ^1[^7LUA^1] ^7powered by "..Author.." ^0^7Visit "..Homepage.." \n\"" )
-		count = {}
-			for i = 0, maxclients - 1 do
-			count[i] = 0
+	gamestate = tonumber( et.trap_Cvar_Get( "gamestate" ) )
+	et.RegisterModname("Hide and Seek by Moe's")
+	if gamestate == 0 then
+		gameActive = true
+		startTime = levelTime + 2000
+		freezeTime = levelTime + (AMOUNT_OF_TIME_TO_FREEZE*1000)
+	end
+end
+
+-- called every server frame
+function et_RunFrame( levelTime )
+	if math.mod(levelTime, samplerate) ~= 0 then return end
+	-- for all clients
+	for clientNum=0, (maxclients - 1) do
+		local team = et.gentity_get(clientNum, "sess.sessionTeam")
+		for k=1, (et.MAX_WEAPONS - 1), 1 do
+			if not weapons[k] then
+				et.gentity_set(clientNum, "ps.ammoclip", k, 0)
+				et.gentity_set(clientNum, "ps.ammo", k, 0)
+				et.gentity_set(clientNum, "ps.powerups", 12, levelTime + 10000 ) -- no fatigue
+				if team == 1 then
+					--et.gentity_set(clientNum, "ps.stats", 4, axisMaxHp)
+				elseif team == 2 then
+					--et.gentity_set(clientNum, "ps.stats", 4, alliedMaxHp)
+					et.gentity_set(clientNum, "ps.ammo", 7, 69)
+				end
+				--et.gentity_set(clientNum, "ps.ammo", 11, 150)
+			end
+		end
+		freezeBlackout(clientNum, levelTime)
+		antigib(clientNum, levelTime)
+	end
+
+end
+
+function et_ClientCommand(client, command)
+	if string.lower(command) == "kill" then
+		return 1
+	end
+	if string.lower(command) == "forcetapout" then
+		return 1
+	end
+	if string.lower(command) == "lock" then
+		return 1
+	end
+	if string.lower(command) == "readyteam" then
+		return 1
+	end
+	if string.lower(command) == "timeout" then
+		return 1
+	end
+	if string.lower(command) == "gethp" then
+		local hp = et.gentity_get(client, "health")
+		local maxhp = et.gentity_get(client, "ps.stats", 4)
+		et.trap_SendServerCommand(client, "chat \"^7Your current hp: " .. hp .. " stat4:" .. maxhp .. "^7\"")
+		return 1
+	end
+	if string.lower(command) == "sethp" then
+		local x = et.trap_Argv(1)
+		et.gentity_set(client, "health", tonumber(x))
+		et.trap_SendServerCommand(client, "chat \"^7Set to " .. x .. "^7\"")
+		return 1
+	end
+	if string.lower(command) == "setmaxhp" then
+		local x = et.trap_Argv(1)
+		et.gentity_set(client, "ps.stats", 4, tonumber(x))
+		et.trap_SendServerCommand(client, "chat \"^7Set to " .. x .. "^7\"")
+		return 1
+	end
+	return 0
+end
+
+function et_ClientSpawn(clientNum, revived)
+	local team = et.gentity_get(clientNum, "sess.sessionTeam")
+	local needles = getAlliedPlayers()
+	if not needles or needles == 0 then needles = 1 end
+	if revived == 0 then
+		--clientSpawnOrigins[clientNum] = et.gentity_get(clientNum, "ps.origin")
+		if team == 1 then -- axis
+			et.gentity_set(clientNum, "ps.stats", 4, axisMaxHp - (axisMaxHp/10)) -- unify max health?
+			et.gentity_set(clientNum, "health", axisMaxHp)
+			et.gentity_set(clientNum, "ps.ammoclip",11, needles)
+		elseif team == 2 then
+			et.gentity_set(clientNum, "ps.stats", 4, alliedMaxHp- (alliedMaxHp/10))
+			et.gentity_set(clientNum, "health", alliedMaxHp)
+		end
+	end
+end
+
+function getAlliedPlayers()
+	local allies = 0
+	for clientNum=0, maxclients-1 do
+		local team = et.gentity_get(clientNum, "sess.sessionTeam")
+		if team == 2 then
+			allies = allies + 1
+		end
+	end
+	return allies
+end
+
+
+function freezeBlackout(clientNum, levelTime)
+	if not gameActive then return end
+	if freezeTime <= levelTime then
+		if not alliesReleasedMessage then
+			et.trap_SendServerCommand( -1, "chat \"^1A^7llies have been released^1!^7\"")
+			alliesReleasedMessage = true
+		end
+		return
+	end
+	if et.gentity_get(clientNum, "sess.sessionTeam") == 2 then
+		et.gentity_set(clientNum, "ps.powerups", 14, levelTime + 500 )
+		et.gentity_set(clientNum, "ps.powerups", 1, levelTime + 5000 )
+		if levelTime >= startTime then
+			if clientSpawnOrigins[clientNum] == 0 then
+				clientSpawnOrigins[clientNum] = et.gentity_get(clientNum, "ps.origin")
+			else
+				et.gentity_set(clientNum, "ps.origin", clientSpawnOrigins[clientNum])
+			end
+		end
+	end
+end
+
+function antigib(clientNum, levelTime)
+	if not antigibEnable then return end
+	local hp = tonumber(et.gentity_get(clientNum,"health"))
+	local body = tonumber(et.gentity_get(clientNum, "r.contents"))
+	if body == 67108864 then
+		et.gentity_set(clientNum, "ps.powerups", 1, levelTime + 9999999 )
+	elseif body == 33554432 and hp >= 76 then
+		et.gentity_set(clientNum, "ps.powerups", 1, 0 )
+	end
+end
+
+function removeEntities()
+	for i = 64, 1021 do
+		local classname = et.gentity_get(i, "classname")
+		if string.find(classname, "mg42", 0, 1) or classname == "func_door_rotating" then
+			et.trap_UnlinkEntity( i )
+		end
+		if mapname == "adlernest" then
+			if et.gentity_get(i, "origin") == "733 -2971 -120" then
+				et.trap_UnlinkEntity( i )
+			end
+		end
 	end
 end
 
